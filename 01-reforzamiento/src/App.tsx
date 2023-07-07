0048d9870cad10040966c8c621debbb0f710af67
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2023-07-07 09:48:24 -0300

    added reforzamientoTSReact

---
+++ 01-reforzamiento/src/App.tsx
@@ -0,0 +1,12 @@
+import { TiposBasicos } from "./typescript/TiposBasicos"
+
+const App = () => {
+  return (
+    <div className='mt-2'>
+      <h1>Introducción a TS - React</h1>
+      <TiposBasicos />
+    </div>
+  )
+}
+
+export default App
